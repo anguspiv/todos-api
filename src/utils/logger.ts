8073c21f9d605c53ba3966aeedf3615508075a8b
Angus <angusp@angusp.com>
2024-05-18 19:34:37 -0700

    feat: adds basic todos api
    Adds basic graphql server for fetching all todos

---
+++ src/utils/logger.ts
@@ -0,0 +1,28 @@
+import { createConsola } from 'consola';
+
+type LevelsMap = {
+  [key: string]: number;
+};
+
+export const LEVELS:LevelsMap = {
+  ERROR: 0,
+  WARN: 1,
+  LOG: 2,
+  INFO: 3,
+  DEBUG: 4,
+  TRACE: 5,
+  SILENT: -999,
+  VERBOSE: 999,
+};
+
+const logLevel = (process.env.LOG_LEVEL || 'LOG').toUpperCase();
+
+const getLogLevel = (level: string): number => LEVELS[level] || LEVELS.INFO;
+
+const level = getLogLevel(logLevel);
+
+export const logger = createConsola({
+  level,
+});
+
+logger.debug('LOG_LEVEL:', logLevel);
